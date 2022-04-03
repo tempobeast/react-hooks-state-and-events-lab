1b88be5a5ca6483c289a6d30ff8be3d049347b64
Zach <shaw.zacht@gmail.com>
2022-04-03 15:23:43 -0700

    commit tree 10a113ebc3ee264037448de1638331c331a8e842

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState("")
+
+  function handleClick() {
+    setIsInCart((isInCart) => !isInCart)
+  }
+
+
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  //const [foodItems, setFoodItems] = useState(items)
+const [selectedCategory, setSelectedCategory] = useState("All")
+
+const foodsToDisplay = items.filter((food) => {
+  if (selectedCategory === "All"){
+    return true;
+  } else {
+    return food.category === selectedCategory;
+  }
+})
+
+
+function handleChange(event) {
+  setSelectedCategory(event.target.value);
+}
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {foodsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
